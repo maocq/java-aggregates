7acab54139880f8e4e5d7a29cefad2d393a84b93
maocq <carmonaesc@gmail.com>
2024-05-10 09:24:43 -0500

    MongoTemplate

--- java-nosql/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/config/MongoConfig.java
+++ java-nosql/infrastructure/driven-adapters/mongo-repository/src/main/java/co/com/bancolombia/mongo/config/MongoConfig.java
@@ -1,11 +1,16 @@
 package co.com.bancolombia.mongo.config;
 
+//import co.com.bancolombia.model.order.Product;
+//import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoConnectionDetails;
 import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.boot.autoconfigure.mongo.PropertiesMongoConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+//import org.springframework.data.mongodb.core.query.Criteria;
+//import org.springframework.data.mongodb.core.query.Query;
 
 @Configuration
 public class MongoConfig {
@@ -23,4 +28,20 @@ public class MongoConfig {
         properties.setUri(secret.getUri());
         return new PropertiesMongoConnectionDetails(properties);
     }
+
+    /*
+    @Bean
+    public String mongoTemplate(MongoClient mongoClient) {
+        //MongoClient mongoClient = MongoClients
+        //        .create(MongoClientSettings.builder()
+        //                .applyConnectionString(new ConnectionString(uri))
+        //                .build());
+
+        ReactiveMongoTemplate template = new ReactiveMongoTemplate(mongoClient, "test");
+        Query query = Query.query(Criteria.where("name").is("Product"));
+
+        var product = template.findOne(query, Product.class).block();
+        return "Ok";
+    }
+    */
 }
